d40bef256c73b592928ccb3236366e529887455f
Никита Кукушкин <nekpom@yandex.ru>
2022-12-07 00:22:42 +0300

    Исправления

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,12 +4,15 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 User = get_user_model()
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    description = models.TextField()        
+    description = models.TextField()
+
     def __str__(self):
         return self.title
+
 
 class Post(models.Model):
     text = models.TextField()
@@ -19,7 +22,7 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
-    group =  models.ForeignKey(
+    group = models.ForeignKey(
         Group,
         on_delete=models.CASCADE,
         related_name='posts',
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,6 +3,7 @@ from .models import Post, Group
 
 # Create your views here.
 
+
 def index(request):
     template = 'posts/index.html'
     posts = Post.objects.order_by('-pub_date')[:10]
@@ -12,6 +13,7 @@ def index(request):
         'posts': posts,
     }
     return render(request, template, context)
+
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
